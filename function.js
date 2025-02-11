5346ddd1edfa7316f759bb747db4371b24697d2d
Santoshi Magar <sntosimagar36@gmail.com>
2025-02-11 14:50:37 +0545

    files

---
+++ function.js
@@ -0,0 +1,15 @@
+//function without parameters
+function addTwoNums () {
+    var a= 10;
+    var b=20;
+    var c=a+b;
+    console.log(c);
+}
+
+//function with parameters
+function addTwoNums (a,b) {
+    var c=a+b;
+    console.log(c);
+}
+
+
